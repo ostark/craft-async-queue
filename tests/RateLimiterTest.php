6e4c8f645dc8a91e120145f2fafa457de2cb3e72
Oliver Stark <os@fortrabbit.com>
2023-06-26 14:52:12 +0200

    Defensive rate limiter: Stop spawning new processes if counting queue jobs failed.

--- tests/RateLimiterTest.php
+++ tests/RateLimiterTest.php
@@ -70,4 +70,23 @@ class RateLimiterTest extends TestCase
 
         $this->assertSame(0, $this->plugin->getRateLimiter()->getInternalCount());
     }
+
+
+    public function test_stop_spawning_processes_when_counting_jobs_failed(): void
+    {
+        // Fake the reserved count
+        $queue = new class extends \craft\queue\Queue {
+            public function getTotalReserved(): int
+            {
+                throw new \Exception('Unable to count jobs');
+            }
+        };
+
+        $this->plugin->set('async_rate_limiter', new \ostark\AsyncQueue\RateLimiter($queue, $this->plugin->getSettings()));
+        $handler = new \ostark\AsyncQueue\Handlers\BackgroundQueueHandler($this->plugin);
+
+        $handler->__invoke(new PushEvent());
+
+        $this->assertFalse($this->plugin->getRateLimiter()->canIUse());
+    }
 }
